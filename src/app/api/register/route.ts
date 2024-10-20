1c41c6f25ec52cc5bf5db2de36590727501a6038
UthpalSuvarna <uthpal33@gmail.com>
2024-10-20 15:44:49 +0530

    Make register form functional

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import z from "zod";
+
+const registerSchema = z.object({
+    username: z.string(),
+    city: z.string(),
+    clinicname: z.string(),
+    userId: z.string()
+})
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        const body = await req.json();  // Parse request body
+        const data = registerSchema.parse(body);
+
+        const result = await prisma.psychiatrist.create({
+            data: {
+                // username: data.username,
+                city: data.city,
+                clinicName: data.clinicname,
+                userId: data.userId
+            }
+        })
+
+
+
+        return NextResponse.json(result);
+    } catch (e: any) {
+        return NextResponse.json({
+            message: "Error" + e.message,
+        });
+    }
+}
